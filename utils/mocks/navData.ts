efe2f97e07e8fabcc57b94a92164b5c968857f85
Patrick448 <patrickcarvalho448@gmail.com>
2022-08-11 18:50:14 -0300

    criada rota cadastrar-instituicao

--- utils/mocks/navData.ts
+++ utils/mocks/navData.ts
@@ -1,4 +1,7 @@
+
 export const navData = [
-  { label: 'Home', path: '/' },
-  { label: 'Instituições', path: '/instituicoes' },
-];
+    {label: 'Home', path: '/'},
+    {label: 'Instituições', path: '/instituicoes'},
+    {label: 'Cadastre-se!', path: '/cadastrar-instituicao'}
+
+]
