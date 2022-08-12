fec11a7a2b8855db026e3570b820b046197c8213
Patrick448 <patrickcarvalho448@gmail.com>
2022-08-12 10:46:59 -0300

    ajustes nos estilos do select

--- styles/globals.ts
+++ styles/globals.ts
@@ -17,7 +17,7 @@ export const GlobalStyle = createGlobalStyle`
         -webkit-font-smoothing: antialiased;
     }
 
-    body, input, textarea, button {
+    body, input, textarea, button, select {
         font-family: 'Poppins', sans-serif;
         font-weight: 400;
         font-size: 1rem;
